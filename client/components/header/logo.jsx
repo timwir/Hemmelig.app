0a9fa1528368c897d49b3384ff17254f40d170ec
Timo Wirkner <109762120+timwir@users.noreply.github.com>
2025-08-26 15:40:14 +0200

    Update logo.jsx

--- client/components/header/logo.jsx
+++ client/components/header/logo.jsx
@@ -1,5 +1,23 @@
 const Logo = ({ className, ...rest }) => (
-   <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" id="b" viewBox="0 0 107.7 83.29" width="107.7"  height="83.29" ><defs><style>.d{fill:#0fd685;stroke:#17e556;stroke-miterlimit:10;}</style></defs><g id="c"><path class="d" d="M90.94,48.09c17.15,23.26,16.71,25.99,16,27.5-7.56,15.94-99.1,4.84-106-27.5C-3.31,28.19,24.15-1.31,45.94.59c10.46.91,22.11,16.45,45,47.5Z"/></g></svg>
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    id="b" 
+    viewBox="0 0 107.7 83.29" 
+    width="107.7" 
+    height="83.29"
+    className={className}
+    {...rest}
+  >
+    <defs>
+      <style>{`.d{fill:#0fd685;stroke:#17e556;stroke-miterlimit:10;}`}</style>
+    </defs>
+    <g id="c">
+      <path 
+        className="d" 
+        d="M90.94,48.09c17.15,23.26,16.71,25.99,16,27.5-7.56,15.94-99.1,4.84-106-27.5C-3.31,28.19,24.15-1.31,45.94.59c10.46.91,22.11,16.45,45,47.5Z"
+      />
+    </g>
+  </svg>
 );
 
 export default Logo;
